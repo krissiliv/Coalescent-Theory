b89d858eb3462de92f1885c00418038c67c624cd
Pia.G <99070866+krissiliv@users.noreply.github.com>
2022-11-22 16:42:39 +0100

    Update Code_msprime.py

--- Code_msprime.py
+++ Code_msprime.py
@@ -12,58 +12,58 @@ num_replicates = 1000 #number of replicates
 
 Lst= [[i,j] for i in np.arange(0,d) for j in np.arange(0,d)] #this is a list of all possible deme pairs
 def give_coal_timeGMM_original(m, d, N, a): #then the function for the coalescent time for the general migration model is defined
-    def M(p):#this is the migration matrix
-        M=np.zeros((p,p)) #first we need a matrix (which is "empty" in the sense that it just contains zeros)
+    def Migration(p):#this is the migration matrix
+        Migration=np.zeros((p,p)) #first we need a matrix (which is "empty" in the sense that it just contains zeros)
         for [i,j] in Lst: #then for each deme pair
             if i==j: 
-                M[i,j]=(1-m) #we set the rate of non-miration 
+                Migration[i,j]=(1-m) #we set the rate of non-miration 
             elif i==j-1 or i==j+1:
-                M[i,j]=a*0.5*m #and the rates of migrating to one of the other demes
+                Migration[i,j]=a*0.5*m #and the rates of migrating to one of the other demes
             elif [i,j]==[0,d-1] or [i,j]==[d-1,0]:
-                M[i,j]=a*0.5*m #this rate should also be used if we look at the two edge demes (as I want to built a circular model => I want to connect the edges)
+                Migration[i,j]=a*0.5*m #this rate should also be used if we look at the two edge demes (as I want to built a circular model => I want to connect the edges)
             else:
-                M[i,j]=(1-a)*(1/(d-3))*m #and this is the rate of global migration (to the non-neighboring demes)
-        return M
-    M=M(d) #m should be d-dimensional
-    def T_h(p): #now I want the code to automatically define the set of variables depending on the number of demes d (which is called p here)
-        T_h=np.zeros((p,p), dtype=dict)
+                Migration[i,j]=(1-a)*(1/(d-3))*m #and this is the rate of global migration (to the non-neighboring demes)
+        return Migration
+    Migration=Migration(d) #m should be d-dimensional
+    def TimeVariable_h(p): #now I want the code to automatically define the set of variables depending on the number of demes d (which is called p here)
+        TimeVariable_h=np.zeros((p,p), dtype=dict)
         for [i,j] in Lst:
             if i>j: #I want to make sure that only entries like T_(12) exist as they are equal to T_(21) entries (that holds because the model is symmetric)
-                T_h[i,j]=sympy.symbols(str('T_' + str(j) + str(i)))
+                TimeVariable_h[i,j]=sympy.symbols(str('T_' + str(j) + str(i)))
             else:
-                T_h[i,j]=sympy.symbols(str('T_' + str(i) + str(j)))
-        return T_h   
-    T=T_h(d) #this is our set of variables. In the ecological sense, the entry T_(12) of this matrix is the coalescence time of two randomly chosen lineages, where one is in deme 1 and the other in deme 2
-    def SumT(p): #now I define sum, which connects all these coalescent times, like in (Nagylaki, 1998): https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1460216/pdf/9649546.pdf
-        SumT=np.zeros((p,p), dtype=dict)
+                TimeVariable_h[i,j]=sympy.symbols(str('T_' + str(i) + str(j)))
+        return TimeVariable_h   
+    TimeVariable=TimeVariable_h(d) #this is our set of variables. In the ecological sense, the entry T_(12) of this matrix is the coalescence time of two randomly chosen lineages, where one is in deme 1 and the other in deme 2
+    def SumTimes(p): #now I define sum, which connects all these coalescent times, like in (Nagylaki, 1998): https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1460216/pdf/9649546.pdf
+        SumTimes=np.zeros((p,p), dtype=dict)
         for [i,j] in Lst:
-            SumT[i,j]=sum(M[i,k]*M[j,l]*T[k,l] for k in np.arange(p) for l in np.arange(p)) - (sum(M[i,k]*M[j,k]*T[k,k]/(2*N) for k in np.arange(p)))
-        return SumT
-    SumT=SumT(d) #this matrix contains the explicit sums for all deme pairs
-    def F_1(p):
-        F_1=np.zeros((p,p), dtype=dict)
+            SumTimes[i,j]=sum(Migration[i,k]*Migration[j,l]*TimeVariable[k,l] for k in np.arange(p) for l in np.arange(p)) - (sum(Migration[i,k]*Migration[j,k]*TimeVariable[k,k]/(2*N) for k in np.arange(p)))
+        return SumTimes
+    SumTimes=SumTimes(d) #this matrix contains the explicit sums for all deme pairs
+    def HelpingFunction_1(p):
+        HelpingFunction_1=np.zeros((p,p), dtype=dict)
         for [i,j] in Lst:
-            F_1[i,j]=(1+SumT[i,j])-T[i,j] #now I set the above defined sums to 0, so that I can later use sympy.solve to solve them after T_(ij) for all deme pairs (ij)
-        return F_1 
-    F=list(F_1(d).reshape(d*d)) 
-    T_list=list(T.reshape(d*d))
-    T_liste1=set(T_list)
-    T_liste=list(T_liste1) #with the last few lines I brought T and F into the right shape so that sympy.solve can work with them
-    F_res=sympy.solve(F,T_liste) #now I can let the variables and the sums run through the "solver", which gives me the appropriate value for each T_(ij), such that all the sums hold true (it solves the equation system)
-    def T_t(p): 
-        T_t=np.zeros((p,p))
+            HelpingFunction_1[i,j]=(1+SumTimes[i,j])-TimeVariable[i,j] #now I set the above defined sums to 0, so that I can later use sympy.solve to solve them after T_(ij) for all deme pairs (ij)
+        return HelpingFunction_1 
+    HelpingFunction=list(HelpingFunction_1(d).reshape(d*d)) 
+    TimeVariables_list=list(TimeVariable.reshape(d*d))
+    TimeVariables_liste1=set(TimeVariables_list)
+    TimeVariables_liste=list(TimeVariables_liste1) #with the last few lines I brought TimeVariables_list and HelpFunction into the right shape so that sympy.solve can work with them
+    HelpingFunction_result=sympy.solve(HelpingFunction,TimeVariables_liste) #now I can let the variables and the sums run through the "solver", which gives me the appropriate value for each T_(ij), such that all the sums hold true (it solves the equation system)
+    def TimesSolution_t(p): 
+        TimesSolution_t=np.zeros((p,p))
         for i in np.arange(0,p):
             for j in np.arange(0,p):
-                T_t[i,j]=F_res[T[i,j]] #here I bring the solution of the equation system in the right shape suhc that I can work with them (I put them all into a matrix)
-        return T_t    
-    T_1=T_t(d)  #Now we have the d-dim matrix of the solutions (it contains each T_(ij) value)
-    S_1=abs(sum(T_1[i,j]/(d*d) for i in np.arange(0,d) for j in np.arange(0,d))) #as my goal is to compare the expected mean coalescence times with each other, I calculate the mean of the coalescence times T_(ij)
+                TimesSolution_t[i,j]=HelpingFunction_result[TimeVariable[i,j]] #here I bring the solution of the equation system in the right shape suhc that I can work with them (I put them all into a matrix)
+        return TimesSolution_t    
+    TimesSolution_1=TimesSolution_t(d)  #Now we have the d-dim matrix of the solutions (it contains each T_(ij) value)
+    Sum_generalModel=abs(sum(TimesSolution_1[i,j]/(d*d) for i in np.arange(0,d) for j in np.arange(0,d))) #as my goal is to compare the expected mean coalescence times with each other, I calculate the mean of the coalescence times T_(ij)
     H=np.zeros(d)
     for h in np.arange(d):
-        H[h]=2*d*N+((d-h)*h)/(2*m) 
-    S_2=np.mean(H) #This has nothing to do with the above, it is a different, less complex formula (for the expected mean coalescence time) of a very restricted model: the Stepping Stone model
-    S_3=(2*d*N + (d-1)*(d-1)/(2*m*d)) #This also has nothing to do with the above, it is a different, less complex formula (for the expected mean coalescence time) of a very restricted model: the Island model 
-    return [int(S_1), int(S_2), int(S_3)]  #GMM,STST,IM
+        H[h]=2*d*N+((d-h)*h)/(2*m) #this formula and the formula in S_3 can be found in (Slatkin, 1991) https://www.cambridge.org/core/services/aop-cambridge-core/content/view/FCC418CBC6F021B741C83FDE6A0E7558/S0016672300029827a.pdf/inbreeding_coefficients_and_coalescence_times.pdf
+    Sum_STSTmodel=np.mean(H) #This has nothing to do with the above, it is a different, less complex formula (for the expected mean coalescence time) of a very restricted model: the Stepping Stone model
+    Sum_Imodel=(2*d*N + (d-1)*(d-1)/(2*m*d)) #This also has nothing to do with the above, it is a different, less complex formula (for the expected mean coalescence time) of a very restricted model: the Island model
+    return [int(Sum_generalModel), int(Sum_STSTmodel), int(Sum_Imodel)]  #GMM,STST,IM
 
 
 
