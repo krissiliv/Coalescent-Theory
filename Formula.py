f01844152983918e89bc0987e339e3ca9307647e
Pia.G <99070866+krissiliv@users.noreply.github.com>
2022-11-22 08:24:25 +0100

    Update Formula.py

--- Formula.py
+++ Formula.py
@@ -2,11 +2,14 @@ import sympy
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 d= 5 #demes
 a= 1 #a% local migration
-N= 1000/d
+N= 1000/d #number of individuals per deme
+#_______________________________________________________
 
 
+#for this function (the steps until line 65) please refer to https://github.com/krissiliv/Coalescent-Theory/blob/main/Code_msprime.py
 Lst= [[i,j] for i in np.arange(0,d) for j in np.arange(0,d)]
 def give_coal_timeGMM_original(m): 
     def M(p): 
@@ -60,36 +63,33 @@ def give_coal_timeGMM_original(m):
         H[h]=2*d*N+((d-h)*h)/(2*m)
     S_2=np.mean(H)
     S_3=(2*d*N + (d-1)*(d-1)/(2*m*d))
-    S_w = 2*N
-    S_b = 2*d*N + (d-1)/(2*m)
-    return [S_1, S_2, S_3, S_w, S_b]  #GMM,STST,IM
+#    S_w = 2*N #this is the within deme coalescence time of the models (I put it here in case I would like to plot it as well, but it is not plotted below)
+#    S_b = 2*d*N + (d-1)/(2*m) #this is the between deme coalescence time of the models (I put it here in case I would like to plot it as well, but it is not plotted below)
+    return [S_1, S_2, S_3] #GMM,STST,IM  #, S_w, S_b]  can also be added - if needed
 
 #---------plotting------------------------------------------------------------------------
-M_emp = [10**i for i in np.linspace(-13, -1, 20)]
+M_emp = [10**i for i in np.linspace(-13, -1, 20)] #this is the range of the parameter m, which represents the migration rate
 
 means_GMM_original = list()
-means_2dN = list()
 means_IM_formula = list()
 means_STST_formula = list()
-means_within_formula = list()
-means_between_formula = list()
 
 for m in M_emp:
     output_GMM_original = give_coal_timeGMM_original(m=m)
-    means_GMM_original.append(output_GMM_original[0])    
-    means_IM_formula.append(output_GMM_original[2])  
-    means_STST_formula.append(output_GMM_original[1])
+    means_GMM_original.append(output_GMM_original[0]) #this is the first entry of line 68 (which is the output of give_coal_timeGMM_original(m))
+    means_IM_formula.append(output_GMM_original[2])  #this is the third entry of the output of give_coal_timeGMM_original(m)
+    means_STST_formula.append(output_GMM_original[1]) #and this is the second one
     
 #-----------------------------------------------------------------------------------------
     
-
+#here I specify the horizontal and vertical axis, the color and shape of the dots/line and the name of it
 plt.plot(M_emp, means_IM_formula, 'b--', label='IM Formula')
 plt.plot(M_emp, means_STST_formula, 'c--', label='STST Formula')
 plt.plot(M_emp, means_GMM_original, 'kD', label='GMM Formula')
 
-plt.legend()
-plt.xlabel("Migration rate");
-plt.ylabel("Mean coalescence time");
-plt.xscale('log');
+plt.legend() #I want to have a legend in the plot
+plt.xlabel("Migration rate"); #this is the name of the horizontal axis
+plt.ylabel("Mean coalescence time"); # this is the name of the vertical axis
+plt.xscale('log'); #this is the scaling of the axis
 plt.yscale('log');
-plt.savefig('F1')
+plt.savefig('F1') #this is the name, with which the plot will be saved in the folder of the code-file
